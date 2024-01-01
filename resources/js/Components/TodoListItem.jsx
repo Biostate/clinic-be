c7c741edf91a2587b57d997e48b35757af36d5f2
Ali Asım <aliasim1625@hotmail.com>
2024-01-01 15:55:18 +0300

    todoListItem-column-update

--- resources/js/Components/TodoListItem.jsx
+++ resources/js/Components/TodoListItem.jsx
@@ -12,24 +12,23 @@ function TodoListItem(props) {
     const dateTextColor = dateTextColorProp === 'green' ? 'text-emerald-600' : dateTextColorProp === 'violet' ? 'text-violet-500' :dateTextColorProp ==='yellow' ? 'text-amber-400': dateTextColorProp ==='pink' ? 'text-pink-600':'bg-blue-100';
   return (
     <div>
-
-        <div className="flex flex-col gap-3 mt-4 ">
+        <div className="flex flex-col gap-3 mt-4  ">
             {/* Satır */}
-            <div className=" flex justify-around bg-neutral-50 p-3 rounded-[10px]">
-                <div className="flex gap-3 mt-2 ">
+            <div className=" flex justify-around ml- bg-neutral-50 p-3 rounded-[10px]">
+                <div className="flex flex-1  gap-3 mt-2 ">
                     <div className={` mt-[1px] w-3 h-3  ${squareColor} rounded-[3px]`}></div>
                     <p className="text-zinc-800 text-xs font-semibold   leading-[18px] tracking-tight  ">
                         {props.title}
                     </p>
                 </div>
-                <div className="mt-2">
+                <div className=" flex mt-2 justify-center flex-1 ">
                     <div className= {`px-2 py-0.5 ${dateBackgroundColor} rounded-2xl `}>
                         <p className={` text-xs font-normal ${dateTextColor} leading-[18px] tracking-tight`}>
                             {props.date}
                         </p>
                     </div>
                 </div>
-                <div className="flex gap-6 ">
+                <div className="flex gap-6 justify-center flex-1 ">
                     <p className="mt-1">Atanan Kişi</p>
                     <Space>
                         <Avatar
@@ -41,6 +40,9 @@ function TodoListItem(props) {
                     </Space>
                 </div>
             </div>
+
+           
+            
         </div>
     </div>
   )
